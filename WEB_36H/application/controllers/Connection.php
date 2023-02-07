2eafa9280a11663710475056e846d4231566d701
SNyAvo <118519118+SNyAvo@users.noreply.github.com>
2023-02-07 15:08:04 +0300

    Model

--- WEB_36H/application/controllers/Connection.php
+++ WEB_36H/application/controllers/Connection.php
@@ -2,5 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Connection extends CI_Controller{
-    
+    public function index(){
+
+    }
+    public function login($utilisateur='',$mdp){
+
+    }
 }
