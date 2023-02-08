a1c76d57b57e3a364a105e489e94bf54ddc8d8be
SNyAvo <118519118+SNyAvo@users.noreply.github.com>
2023-02-08 14:39:39 +0300

    metier

--- WEB36H/application/controllers/Objet.php
+++ WEB36H/application/controllers/Objet.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Objet extends CI_Controller {	
     
-    public function objet($prix)
+    /*public function objet($prix)
     {
         $this->load->model('Objet_Model');
         $adm['objet']=$this->Objet_Model->liste_proposition_objet($prix);
@@ -14,7 +14,10 @@ class Objet extends CI_Controller {
         $this->load->model('Objet_Model');
         $adm['object']=$this->Objet_Model->liste_objet_categorie($idcategorie);
         $this->load->view('profil',$adm);
-    }
-
+    }*/
+    /*public function cherche(){
+        $this->load->model('Objet_Model');
+        $this->Objet_Model->search(1,"phone");
+    }*/
         
 }
